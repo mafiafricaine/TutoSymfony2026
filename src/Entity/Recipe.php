307584c17ae295cab377690846a0cd830211a305
julienDunia <juliendunia@hotmail.com>
2026-06-10 10:04:44 +0200

    ajout de la relation entre User et Recipe

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -46,6 +46,10 @@ class Recipe
     #[ORM\Column(length: 500, nullable: true)]
     private ?string $imageName = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
 
+    #[ORM\ManyToOne(inversedBy: 'recipes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -109,6 +113,18 @@ class Recipe
     public function setImageName(?string $imageName): static
     {
         $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
